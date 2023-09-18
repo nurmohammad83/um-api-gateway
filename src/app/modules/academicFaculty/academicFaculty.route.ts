feaed7ffa33ec5c3d2d20b9e0ad304fe69c31749
Nur Mohammad <mohammadnur5480@gmail.com>
2023-09-18 10:33:49 +0600

    feat: connect with redis and create acdemi faculty data

--- src/app/modules/academicFaculty/academicFaculty.route.ts
+++ src/app/modules/academicFaculty/academicFaculty.route.ts
@@ -1,12 +1,31 @@
 import express from 'express';
 import { AcademicFacultyController } from './academicFaculty.controller';
+import validateRequest from '../../middlewares/validateRequest';
+import { AcademicFacultyValidation } from './academicFaculty.validation';
+import auth from '../../middlewares/auth';
+import { ENUM_USER_ROLE } from '../../../enums/user';
 
 const router = express.Router();
 
 router.get('/', AcademicFacultyController.getAllFromDb);
 router.get('/:id', AcademicFacultyController.getByIdFromDb);
-router.post('/', AcademicFacultyController.insertIntoDB);
-router.delete('/:id', AcademicFacultyController.deleteByIdFromDb);
-router.patch('/:id', AcademicFacultyController.updateOneIntoDb);
+router.post(
+  '/',
+  validateRequest(AcademicFacultyValidation.create),
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),
+  AcademicFacultyController.insertIntoDB
+);
+router.delete(
+  '/:id',
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  AcademicFacultyController.deleteByIdFromDb
+);
+
+router.patch(
+  '/:id',
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  validateRequest(AcademicFacultyValidation.update),
+  AcademicFacultyController.updateOneIntoDb
+);
 
 export const AcademicFacultyRoutes = router;
--- src/app/modules/academicSemester/academicSemester.route.ts
+++ src/app/modules/academicSemester/academicSemester.route.ts
@@ -1,10 +1,28 @@
 import express from 'express';
 import { AcademicSemesterController } from './academicSemester.controller';
+import validateRequest from '../../middlewares/validateRequest';
+import { AcademicSemesterValidation } from './academicSemester.validations';
+import auth from '../../middlewares/auth';
+import { ENUM_USER_ROLE } from '../../../enums/user';
 const router = express.Router();
 
 router.get('/', AcademicSemesterController.getAllFromDb);
 router.get('/:id', AcademicSemesterController.getByIdFromDb);
-router.post('/', AcademicSemesterController.insertIntoDB);
-router.delete('/:id', AcademicSemesterController.deleteByIdFromDb);
-router.patch('/:id', AcademicSemesterController.updateOneIntoDb);
+router.post(
+  '/',
+  validateRequest(AcademicSemesterValidation.create),
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),
+  AcademicSemesterController.insertIntoDB
+);
+router.delete(
+  '/:id',
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),
+  AcademicSemesterController.deleteByIdFromDb
+);
+router.patch(
+  '/:id',
+  validateRequest(AcademicSemesterValidation.update),
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),
+  AcademicSemesterController.updateOneIntoDb
+);
 export const AcademicSemesterRoutes = router;
